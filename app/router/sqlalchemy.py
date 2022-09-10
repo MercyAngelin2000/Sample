81d7b9ed7d4586d7c7a421cce5f2bd1c8b250ab0
MercyAngelin <mercyangelin2000@gmail.com>
2022-09-10 10:14:21 +0530

    comment post

--- app/router/sqlalchemy.py
+++ app/router/sqlalchemy.py
@@ -7,49 +7,49 @@ from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
-@router.get("/sqlalchemy")
-def testpost(db : Session = Depends(get_db)):
-    p=db.query(models.Fastapisample).all()
-    return {"msg":p}
+# @router.get("/sqlalchemy")
+# def testpost(db : Session = Depends(get_db)):
+#     p=db.query(models.Fastapisample).all()
+#     return {"msg":p}
 
-@router.post("/sqlcreate",status_code=status.HTTP_201_CREATED,response_model=schema.Postmethod)
-def createdb(Post:schema.sqlmethod ,db : Session = Depends(get_db)): 
-    # new = models.Fastapisample(title=Post.title,content=Post.content,published=Post.published)
-    new = models.Fastapisample(**Post.dict())
-    db.add(new)
-    db.commit()
-    db.refresh(new)
-    return new
+# @router.post("/sqlcreate",status_code=status.HTTP_201_CREATED,response_model=schema.Postmethod)
+# def createdb(Post:schema.sqlmethod ,db : Session = Depends(get_db)): 
+#     # new = models.Fastapisample(title=Post.title,content=Post.content,published=Post.published)
+#     new = models.Fastapisample(**Post.dict())
+#     db.add(new)
+#     db.commit()
+#     db.refresh(new)
+#     return new
 
-@router.get("/sqlget/{id}")
-def getposts(id: int,db : Session = Depends(get_db)):
-    p=db.query(models.Fastapisample).filter(models.Fastapisample.id== id).all()
-    if not p:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id:{id} was not found")
-    return {"postdetail":p}
+# @router.get("/sqlget/{id}")
+# def getposts(id: int,db : Session = Depends(get_db)):
+#     p=db.query(models.Fastapisample).filter(models.Fastapisample.id== id).all()
+#     if not p:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id:{id} was not found")
+#     return {"postdetail":p}
 
-@router.delete("/sqldel/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def deletepost(id:int,db : Session = Depends(get_db)):
-    p=db.query(models.Fastapisample).filter(models.Fastapisample.id== id)
-    if p.first() == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+# @router.delete("/sqldel/{id}",status_code=status.HTTP_204_NO_CONTENT)
+# def deletepost(id:int,db : Session = Depends(get_db)):
+#     p=db.query(models.Fastapisample).filter(models.Fastapisample.id== id)
+#     if p.first() == None:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
 
-    p.delete(synchronize_session=False)
-    db.commit()
+#     p.delete(synchronize_session=False)
+#     db.commit()
     
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+#     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@router.put("/sqlupdate/{id}")
-def updatepost(id: int, Post:schema.sqlmethod,db : Session = Depends(get_db)):
-    p=db.query(models.Fastapisample).filter(models.Fastapisample.id== id)
-    data = p.first()
+# @router.put("/sqlupdate/{id}")
+# def updatepost(id: int, Post:schema.sqlmethod,db : Session = Depends(get_db)):
+#     p=db.query(models.Fastapisample).filter(models.Fastapisample.id== id)
+#     data = p.first()
 
-    if data == None:        
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
+#     if data == None:        
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
     
-    p.update(Post.dict(),synchronize_session=False)
-    db.commit()
-    return {"Msg":p.first()} 
+#     p.update(Post.dict(),synchronize_session=False)
+#     db.commit()
+#     return {"Msg":p.first()} 
 
 @router.post("/login",response_model=schema.Token)
 def login(data: OAuth2PasswordRequestForm = Depends(),db : Session = Depends(get_db)):
